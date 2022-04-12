32402b4389cdc60c466d84e720f631e84847743a
Iliyan <iliyan.vlahov@gmail.com>
2022-04-12 20:39:17 +0300

    prettify

--- src/QueryNavLink.tsx
+++ src/QueryNavLink.tsx
@@ -1,6 +1,6 @@
-import { useLocation, NavLink, NavLinkProps } from 'react-router-dom';
+import { useLocation, NavLink, NavLinkProps } from 'react-router-dom'
 
 export default function QueryNavLink({ to, ...props }: NavLinkProps) {
-  let location = useLocation();
-  return <NavLink to={to + location.search} {...props} />;
+  let location = useLocation()
+  return <NavLink to={to + location.search} {...props} />
 }
