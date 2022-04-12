0dfb2a3fcfb90a1a127b92bfee55bdcbf7337084
Iliyan <iliyan.vlahov@gmail.com>
2022-04-12 23:22:15 +0300

    simple hardcoded card with mui

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,1 +1,42 @@
-export default function Card() {}
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import { AccessTime } from '@mui/icons-material'
+import { Rating } from '@mui/material'
+
+export default function Card() {
+  return (
+    <Paper elevation={3}>
+      <img
+        src="https://assets.simpleviewcms.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg"
+        alt="some view from the internet"
+        className="img"
+      />
+      <Box paddingX={1}>
+        <Typography component="h2" variant="subtitle1">
+          Immerse into the Falls
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <AccessTime sx={{ width: 16 }} />
+          <Typography variant="body2" component="p" marginLeft={0.5}>
+            5 hours
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }} marginTop={3}>
+          <Rating size="small" name="read-only" value={4.5} readOnly precision={0.5}></Rating>
+          <Typography variant="body2" component="p" marginLeft={0.5}>
+            4.5
+          </Typography>
+          <Typography variant="body2" component="p" marginLeft={1.5}>
+            (655 reviews)
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" component="h3">
+            From C $147
+          </Typography>
+        </Box>
+      </Box>
+    </Paper>
+  )
+}
--- src/routes/tours.tsx
+++ src/routes/tours.tsx
@@ -1,3 +1,29 @@
+import { CssBaseline } from '@mui/material'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Card from '../Components/Card'
+
 export default function Tours() {
-  return <div>Hello world</div>
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={3}>
+            <Card></Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card></Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card></Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card></Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
 }
